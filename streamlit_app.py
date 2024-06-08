9aec96f5cfe43971444033be88c8e219ad7d8913
satoudesita <167180203+satoudesita@users.noreply.github.com>
2024-06-08 07:07:04 +0000

    ああ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-
+from PIL import Image
 # Excelファイルのパス
 EXCEL_FILE_PATH = "国.xlsx"
 
@@ -18,11 +17,16 @@ def show_country_info(country_data):
         st.write(f"国名: {country_name}")
         country_description = country_data.iloc[0]['人口']
         st.write(f"説明: {country_description}")  # 説明を表示
-        country_image_url = country_data.iloc[0]['画像']
-        if country_image_url:
-            st.image(country_image_url, caption='国の画像', use_column_width=True)
+        if '画像' in country_data.columns:
+            country_image_url = country_data.iloc[0]['画像']
+            if country_image_url:
+                st.image(country_image_url, caption='国の画像', use_column_width=True)
+        else:
+            st.write('画像の列が存在しません。')
     else:
         st.write('入力された国の情報が見つかりませんでした。')
+
+
 
 # メインのStreamlitアプリケーション
 def main():
