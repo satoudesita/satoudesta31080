c1f54aff35733f07c866c5b70b361d3e0f04eff0
satoudesita <167180203+satoudesita@users.noreply.github.com>
2025-01-29 06:57:36 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,198 +1,255 @@
+import sqlite3
 import streamlit as st
-import pandas as pd
-import numpy as np
-import time
 
-# ページタイトルの設定
-st.set_page_config(page_title="歴史問題")
-
-# タイトルと説明
-st.title('歴史問題')
-
-# CSSを適用して背景色を設定
-st.write(
-    """
-    <style>
-        .stApp {
-            background-color: #f8f8ff;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-# データの読み込み
-@st.cache_data
-def load_data():
-    return pd.read_excel("rekisi.xlsx")
-
-words_df = load_data()
-
-# タブの選択（サイドバーに配置）
-tabs = st.sidebar.radio("選択してください", ['使い方','難易度１', '難易度２', '難易度３'])
-
-# 正解数と不正解数のカウンターをセッション状態で初期化
-if 'correct_answers' not in st.session_state:
-    st.session_state.correct_answers = 0
-
-if 'incorrect_answers' not in st.session_state:
-    st.session_state.incorrect_answers = 0
-
-# タイマーの設定
-def start_timer(ti):
-    st.session_state.start_time = time.time()
-    st.session_state.timer_active = True
-
-def update_timer(ti):
-    elapsed_time = time.time() - st.session_state.start_time
-    remaining_time = max(0, ti - elapsed_time)
-    return remaining_time
-
-# 不正解数が制限に達したときの処理
-def end_game():
-    # 難易度によってスコアの倍率を変更
-    multiplier = 1
-    if tabs == '難易度２':
-        multiplier = 1.5
-    elif tabs == '難易度３':
-        multiplier = 2
-
-    final_score = st.session_state.correct_answers * int(30 / ti * 10) * multiplier
-
-    st.markdown(
-        f"""
-        <div style="height: 100vh; display: flex; flex-direction: column; align-items: center; justify-content: center;">
-            <h1 style="color: red; font-size: 50px; text-align: center;">終了</h1>
-            <div style="background-color: #ADD8E6; padding: 20px; border-radius: 10px; margin-top: 20px;">
-                <h2 style="color: black; text-align: center;">最終結果</h2>
-                <p style="color: black; font-size: 24px; text-align: center;">正解数: {st.session_state.correct_answers}</p>
-                <p style="color: black; font-size: 24px; text-align: center;">不正解数: {st.session_state.incorrect_answers}</p>
-                <p style="color: black; font-size: 24px; text-align: center;">計算結果: {final_score}</p>
-            </div>
-        </div>
-        """,
-        unsafe_allow_html=True
+# SQLiteデータベースの接続とテーブルの作成
+def create_user_table():
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL
     )
+    """)
+    
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS buttons (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL,
+        button_name TEXT NOT NULL,
+        button_color TEXT NOT NULL DEFAULT 'lightgray',
+        is_pressed INTEGER NOT NULL DEFAULT 0
+    )
+    """)
+    conn.commit()
+    conn.close()
 
-    if st.button('もう一度', key='reset_button'):
-        st.session_state.correct_answers = 0
-        st.session_state.incorrect_answers = 0
-        st.experimental_rerun()  # ページのリロード
-    st.stop()
+def add_button_color_column():
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+    try:
+        c.execute("ALTER TABLE buttons ADD COLUMN button_color TEXT NOT NULL DEFAULT 'lightgray'")
+        conn.commit()
+    except sqlite3.OperationalError as e:
+        print(f"Error: {e}")
+    finally:
+        conn.close()
 
-def display_timer(ti):
-    remaining_time = update_timer(ti)
-    timer_placeholder = st.empty()
-    while remaining_time > 0:
-        remaining_time = update_timer(ti)
-        timer_placeholder.markdown(
-            f'<h2 style="color: #20b2aa; font-size: 36px; text-align: center;">残り時間: {remaining_time:.1f} 秒</h2>',
-            unsafe_allow_html=True
-        )
-        time.sleep(0.1)
-    if not st.session_state.quiz_answered:
-        st.session_state.incorrect_answers += 1
-        st.error(f"時間切れです。正解は {st.session_state.correct_answer} でした。")
+def sign_up(username, password):
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+    try:
+        c.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+        conn.commit()
+        st.success("サインアップ成功！")
+    except sqlite3.IntegrityError:
+        st.error("そのユーザー名はすでに使われています。")
+    finally:
+        conn.close()
 
-if tabs in ['使い方']:
-    st.text("ガチャについて")
-    st.text("1. 問題を解く制限時間を設定する（短いほど得点の倍率が高い）")
-    st.text("2. ガチャを引くボタンを押す")
-    st.text("3. 出てきた問題に対応する年号を4つの年号が書かれている選択肢ボタンから選ぶ")
-    st.text("注意（時間制限を過ぎると不正解扱いになります）")
-    st.text("4. もう一度ガチャを引くボタンを押し、再度問題を解く")
-    st.text("5. 不正解数が5(難易度1の時)を超えると終了となり、それまでの正解数、得点が出ます。")
-    st.text("得点の仕様　正解数×30÷制限時間")
-    st.text("難易度１：１倍　難易度２：1.5倍　難易度３：2倍")
-if tabs in ['難易度１', '難易度２', '難易度３']:
-    # スライダーとガチャボタンの表示
-    ti = st.slider('時間制限を選択してください。小さいほど得点は高いです', min_value=5, max_value=30, value=15, step=5)
-    if ti == 0:
-        st.text('0は選択できません')
-        st.stop()
+def sign_in(username, password):
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+    c.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
+    user = c.fetchone()
+    conn.close()
+    return user
 
-    if st.session_state.correct_answers < 5:
-        m = 3
-    elif st.session_state.correct_answers < 10:
-        m = 5
+def load_buttons(username):
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+    c.execute("SELECT id, button_name, button_color, is_pressed FROM buttons WHERE username = ?", (username,))
+    buttons = c.fetchall()
+    conn.close()
+    return buttons
+
+def save_button(username, button_name, button_color):
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+    c.execute("INSERT INTO buttons (username, button_name, button_color) VALUES (?, ?, ?)", (username, button_name, button_color))
+    conn.commit()
+    conn.close()
+
+def delete_button(username, button_name):
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+    c.execute("DELETE FROM buttons WHERE username = ? AND button_name = ?", (username, button_name))
+    conn.commit()
+    conn.close()
+
+def update_button_state(username, button_name):
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+    c.execute("UPDATE buttons SET is_pressed = 1 WHERE username = ? AND button_name = ?", (username, button_name))
+    conn.commit()
+    conn.close()
+
+def reset_button_state(username):
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+    c.execute("UPDATE buttons SET is_pressed = 0 WHERE username = ?", (username,))
+    conn.commit()
+    conn.close()
+
+def create_fruit_table():
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS fruits (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT UNIQUE NOT NULL
+    )
+    """)
+    conn.commit()
+    conn.close()
+
+def add_fruit(fruit_name):
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+    try:
+        c.execute("INSERT INTO fruits (name) VALUES (?)", (fruit_name,))
+        conn.commit()
+        st.success(f"{fruit_name}が追加されました！")
+    except sqlite3.IntegrityError:
+        st.error(f"{fruit_name}は既に存在します。")
+    finally:
+        conn.close()
+
+def load_fruits():
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+    c.execute("SELECT name FROM fruits")
+    fruits = c.fetchall()
+    conn.close()
+    return [fruit[0] for fruit in fruits]
+
+# 初期化処理（家事の進行中タスクを管理するためのtasks属性を追加）
+if 'tasks' not in st.session_state:
+    st.session_state.tasks = {}
+
+# セッション状態でログイン情報を管理
+if 'logged_in' not in st.session_state:
+    st.session_state.logged_in = False
+if 'username' not in st.session_state:
+    st.session_state.username = None
+
+def create_buttons():
+    name = st.text_input("家事を追加:")
+    button_color = st.color_picker("ボタンの色を選んでください:", "#FF6347")
+
+    # 家事の追加
+    if name:
+        if st.button("家事を追加"):
+            save_button(st.session_state.username, name, button_color)
+
+    # 進行中の家事を表示
+    cols = st.columns(4)
+    for i, (button_id, button_name, button_color, is_pressed) in enumerate(load_buttons(st.session_state.username)):
+        col_index = i % 4
+        with cols[col_index]:
+            button_label = f"{button_name}"
+            button_html = f"""
+            <button style="background-color:{button_color}; width: 100%; height: 50px; border: none; color: white; font-size: 16px;">
+                {button_label}
+            </button>
+            """
+            if is_pressed:
+                st.markdown(button_html, unsafe_allow_html=True)
+                st.write(f"{button_name}のボタンが押されました！")
+                
+                # ユーザーごとに進行中の家事を管理
+                if 'tasks' not in st.session_state:
+                    st.session_state.tasks = {}
+                if st.session_state.username not in st.session_state.tasks:
+                    st.session_state.tasks[st.session_state.username] = []
+
+                # 進行中の家事に追加
+                if button_name not in st.session_state.tasks[st.session_state.username]:
+                    st.session_state.tasks[st.session_state.username].append(button_name)
+            else:
+                if st.button(button_label, key=f"button_{button_id}"):
+                    update_button_state(st.session_state.username, button_name)
+                    st.write(f"{button_name}のボタンがクリックされました！")
+    
+    # 家事のリセット
+    if st.button("家事をリセット"):
+        reset_button_state(st.session_state.username)
+        st.session_state.tasks = {}  # 進行中の家事もリセット
+        st.success("全ての家事がリセットされました。")
+    
+    # ユーザーごとの進行中の家事を表示
+    st.write("進行中の家事:")
+    
+    # ユーザーを選択する
+    fruits = load_fruits()
+    option = st.selectbox('ユーザーを選んで下さい', fruits if fruits else ['追加して'], key=f"user_selectbox_{st.session_state.username}")
+
+    st.write(f'選択されたユーザー {option} の進行中の家事:')
+    
+    # 選択されたユーザーの進行中の家事を表示
+    if option in st.session_state.tasks:
+        tasks = st.session_state.tasks[option]
+        for task in tasks:
+            st.write(f"・ {task}")
     else:
-        m = 7
+        st.write("進行中の家事はありません。")
 
-    # 不正解数が制限に達した場合のゲーム終了
-    incorrect_limit = 5 if tabs == '難易度１' else (3 if tabs == '難易度２' else 1)
-    if st.session_state.incorrect_answers >= incorrect_limit:
-        end_game()
 
-    # ガチャ機能
-    if st.button('ガチャを引く！', key='gacha_button'):
-        rarity_probs = {'N': 1.0}
-        chosen_rarity = np.random.choice(list(rarity_probs.keys()), p=list(rarity_probs.values()))
-        subset_df = words_df[words_df['難易度'] == chosen_rarity]
-        selected_word = subset_df.sample().iloc[0]
+def sign_in_screen():
+    username = st.sidebar.text_input("ユーザー名")
+    password = st.sidebar.text_input("パスワード", type="password")
+    
+    if st.sidebar.button("ログイン"):
+        user = sign_in(username, password)
+        if user:
+            st.session_state.logged_in = True
+            st.session_state.username = username
+            st.success("ログイン成功！")
+        else:
+            st.sidebar.error("ユーザー名またはパスワードが間違っています。")
 
-        other_words = words_df[words_df['問題'] != selected_word['問題']].sample(m)
-        choices = other_words['回答'].tolist() + [selected_word['回答']]
-        np.random.shuffle(choices)
+def sign_up_screen():
+    username = st.sidebar.text_input("新しいユーザー名")
+    password = st.sidebar.text_input("新しいパスワード", type="password")
+    confirm_password = st.sidebar.text_input("パスワード確認", type="password")
+    
+    if st.sidebar.button("サインアップ"):
+        if password != confirm_password:
+            st.sidebar.error("パスワードが一致しません。")
+        else:
+            sign_up(username, password)
 
-        st.session_state.selected_word = selected_word
-        st.session_state.choices = choices
-        st.session_state.correct_answer = selected_word['回答']
-        st.session_state.display_meaning = False
-        st.session_state.quiz_answered = False
-        start_timer(ti)
+def home_screen():
+    if st.session_state.logged_in:
+        st.write(f"ログイン中: {st.session_state.username}さん")
 
-    if 'selected_word' in st.session_state:
-        st.header(f"問題: {st.session_state.selected_word['問題']}")
-        st.subheader(f"難易度: {st.session_state.selected_word['難易度']}")
+        fruits = load_fruits()
 
-        if not st.session_state.quiz_answered and st.session_state.timer_active:
-            st.write("この問題の年号はどれでしょう？")
+        option = st.selectbox('ユーザーを選んで下さい', fruits if fruits else ['追加して'], key="user_selectbox")
 
-            num_choices = len(st.session_state.choices)
-            num_cols = 4
-            num_rows = (num_choices + num_cols - 1) // num_cols
+        st.write(f'あなたが選んだユーザーは: {option}')
+        
+        new_fruit = st.text_input("新しいユーザーを追加:")
+        if st.button("ユーザーを追加"):
+            if new_fruit:
+                add_fruit(new_fruit)
+            else:
+                st.error("ユーザーの名前を入力してください。")
 
-            for row in range(num_rows):
-                cols = st.columns(num_cols)
-                for col in range(num_cols):
-                    choice_index = row * num_cols + col
-                    if choice_index < num_choices:
-                        choice = st.session_state.choices[choice_index]
-                        if cols[col].button(str(choice), key=f'choice_{row}_{col}'):
-                            st.session_state.selected_choice = choice
-                            st.session_state.quiz_answered = True
-                            st.session_state.timer_active = False
+        create_buttons()
+    else:
+        st.write("左のサイドバーからサインインまたはサインアップを選択してください。")
 
-                            if choice == st.session_state.correct_answer:
-                                st.session_state.correct_answers += 1
-                                st.success("正解です！")
-                            else:
-                                st.session_state.incorrect_answers += 1
-                                st.error(f"不正解です。正解は {st.session_state.correct_answer} でした。")
+create_user_table()
+add_button_color_column()
+create_fruit_table()
 
-                            st.session_state.display_meaning = True
-        elif st.session_state.quiz_answered or not st.session_state.timer_active:
-            st.write("回答済みです。次の問題に進んでください。")
+menu = st.sidebar.radio("メニュー", ["ホーム", "サインイン", "サインアップ"])
 
-    if 'timer_active' in st.session_state and st.session_state.timer_active:
-        display_timer(ti)
-
-    # 正解数と不正解数を表示
-    col1, col2 = st.columns(2)
-    with col1:
-        st.markdown(
-            f'<div style="background-color: #ADD8E6; padding: 5px; border-radius: 3px;">'
-            f'<h3 style="color: black;">正解した数</h3>'
-            f'<p style="color: black; font-size: 24px; text-align: center;">{st.session_state.correct_answers}</p>'
-            f'</div>',
-            unsafe_allow_html=True
-        )
-
-    with col2:
-        st.markdown(
-            f'<div style="background-color: #ADD8E6; padding: 5px; border-radius: 3px;">'
-            f'<h3 style="color: black;">不正解した数</h3>'
-            f'<p style="color: black; font-size: 24px; text-align: center;">{st.session_state.incorrect_answers}</p>'
-            f'</div>',
-            unsafe_allow_html=True
-        )
+if menu == "ホーム":
+    home_screen()
+elif menu == "サインイン":
+    sign_in_screen()
+elif menu == "サインアップ":
+    sign_up_screen()
