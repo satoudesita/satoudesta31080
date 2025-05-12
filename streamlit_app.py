e7bc308f2699d41755a914d431a010a7af5fd418
satoudesita <167180203+satoudesita@users.noreply.github.com>
2025-05-12 07:39:04 +0000

    ｓ

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ seconds_left = int(remaining.total_seconds())
 
 st.markdown(
     f"""
-    <div style="text-align: center; padding-top: 210px;">
+    <div style="text-align: center; padding-top: 170px;">
         <h2 style="font-size: 96px; color: #333;">出席コード</h2>
         <h1 style="font-size: 300px; color: #004080;">{st.session_state.code}</h1>
         <p style="font-size: 72px; color: #666;">次の更新まで: {seconds_left} 秒</p>
